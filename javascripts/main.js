02235c38d360f0100985f33d8dcb697f4289951c
Andy <amillion3@gmail.com>
2018-06-21 21:58:59 -0500

    WIP, to do list in main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,3 +3,11 @@ const data = require ('./data');
 
 apiKeys.retrieveKeys();
 data.initializer();
+
+// TO DO
+// Fix blog posts grab, get in order
+// Figure out how to hide content behind the footer in regular view
+// 'Active' buttons/tabs for the page you are on
+// Increase menu buttons font size
+// Container for blog posts (white transparent?)
+// Do contact page
